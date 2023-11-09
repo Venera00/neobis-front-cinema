8915eed18a4afe21fe04d033f369e9d4b9fe09d9
Venera <veneraomurzakkyzy@gmail.com>
2023-11-09 14:40:38 +0600

    fix movie card in css and add function of getting, showing data

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const API_KEY = "7f040a3b-2c46-4b61-a7f9-7726d3d1a082";
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+
+getMovies(API_URL_POPULAR);
+async function getMovies(url) {
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+  showMovies(respData);
+}
+
+function showMovies(data) {
+  const moviesEl = document.querySelector(".section-movies");
+
+  data.films.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie-wrapper");
+    movieEl.innerHTML = `
+    <img
+          src="${movie.posterUrlPreview}"
+          alt="${movie.nameRu}"
+          class="movie__img"
+        />
+        <div class="movie-info__wrapper">
+          <div class="movie__info">
+            <div class="movie__title">${movie.nameRu}</div>
+            <div class="movie__year">${movie.year}</div>
+            <div class="movie__genre">${movie.genres.map(
+              (genre) => `${genre.genre}`
+            )}</div>
+          </div>
+          <div class="movie__rating movie__rating-green">8</div>
+          <div class="movie-rate__wrapper">
+            <button href="#" class="heart"></button>
+          </div>
+        </div>
+    `;
+    moviesEl.appendChild(movieEl);
+  });
+}
