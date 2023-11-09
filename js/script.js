1943ab39c29e12282b653314dd6767271172824b
Venera <veneraomurzakkyzy@gmail.com>
2023-11-10 02:50:57 +0600

    add search function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const API_KEY = "7f040a3b-2c46-4b61-a7f9-7726d3d1a082";
 const API_URL_POPULAR =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
 getMovies(API_URL_POPULAR);
 async function getMovies(url) {
@@ -14,8 +16,20 @@ async function getMovies(url) {
   showMovies(respData);
 }
 
+function getByRate(rate) {
+  if (rate >= 7) {
+    return "green";
+  } else if (rate >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
 function showMovies(data) {
   const moviesEl = document.querySelector(".section-movies");
+
+  document.querySelector(".section-movies").innerHTML = "";
 
   data.films.forEach((movie) => {
     const movieEl = document.createElement("div");
@@ -32,7 +46,9 @@ function showMovies(data) {
             <div class="movie__year">${movie.year}</div>
             <div class="movie__genre">${movie.genres[0].genre}</div>
           </div>
-          <div class="movie__rating movie__rating-green">${movie.rating}</div>
+          <div class="movie__rating movie__rating-${getByRate(movie.rating)}">${
+      movie.rating
+    }</div>
           <div class="movie-rate__wrapper">
             <button href="#" class="heart"></button>
           </div>
@@ -41,3 +57,17 @@ function showMovies(data) {
     moviesEl.appendChild(movieEl);
   });
 }
+
+const form = document.querySelector("form");
+const searchInput = document.querySelector(".header__search");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchUrl = `${API_URL_SEARCH}${searchInput.value}`;
+  if (searchInput.value) {
+    getMovies(searchUrl);
+
+    searchInput.value = "";
+  }
+});
